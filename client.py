bc7ec963abe07820ab02dbd0f97450eac9adef73
jeslinjoy <jeslin@qburst.com>
2015-07-14 16:39:28 +0530

    jeslin

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = 'localhost'    #server name goes in here
+PORT = 3820             
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST,PORT))
+with open('myUpload.txt', 'rb') as file_to_send:
+    for data in file_to_send:
+        socket.sendall(data)
+print 'end'
+socket.close()
